075db55adab2f7d64dcb1c211e4c49f3fe33794d
arielisr <rel.isr@gmail.com>
2014-07-10 23:22:13 +0300

    adding plot3

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-## Create Plot2
+## Create Plot3
 ## Exploratory data analysis course, Johns Hopkins/coursera
 ## Ariel Israel, Jul 2014
 
@@ -9,6 +9,9 @@ data=data[data$Date %in% c('1/2/2007','2/2/2007'),]
 # build the date-time from strings
 data$DateTime=strptime(paste(data$Date,data$Time),'%d/%m/%Y %H:%M:%S')
 # draw the plot
-png(filename = "plot2.png", width = 500, height = 500)
-plot(data$DateTime,data$Global_active_power,type='l',ylab='Global Active Power (Kilowatts)',xlab='')
+png(filename = "plot3.png", width = 500, height = 500)
+plot(data$DateTime,data$Sub_metering_1,type='l',ylab='Energy sub metering',xlab='',col='black')
+lines(data$DateTime,data$Sub_metering_2,col='red')
+lines(data$DateTime,data$Sub_metering_3,col='blue')
+legend("topright", pch='_', col = c("black","red","blue"), legend = paste("Sub_metering",1:3,sep='_'))
 dev.off()
