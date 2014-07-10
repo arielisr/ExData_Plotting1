8280580ee06cbe546d126fae2909dbb12de27cce
arielisr <dr.ariel.israel@gmail.com>
2014-07-10 22:52:15 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,15 @@
 ## Create Plot1
 ## Exploratory data analysis course, Johns Hopkins/coursera
+## Ariel Israel, Jul 2014
 
-# read the data
-setwd('d:/datasets/exploratory_data_analysis')
+# set the directory and read the data
+setwd('D:/courses/exploratory_data_analysis')
 data=read.csv('household_power_consumption.txt',sep=';',na.strings='?')
+# select the dates 1.2.2007 and 2.2.2007
 data=data[data$Date %in% c('1/2/2007','2/2/2007'),]
+# convert the date
 data$Date=strptime(data$Date,'%d/%m/%Y')
+# draw the histogram
+png(filename = "plot1.png", width = 500, height = 500)
 hist(data$Global_active_power,col='#ff2500',xlab='Global Active Power (Kilowatts)',main = "Global Active Power")
-dev.copy(png, file = "plot1.png")
 dev.off()
