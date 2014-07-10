90f86afb75d5dbe42dc4da824229f8d6fb07182c
arielisr <rel.isr@gmail.com>
2014-07-10 23:33:35 +0300

    adding plot3

--- plot4.R
+++ plot4.R
@@ -6,18 +6,18 @@
 data=read.csv('D:/datasets/exploratory_data_analysis/household_power_consumption.txt',sep=';',na.strings='?')
 # select the dates 1.2.2007 and 2.2.2007
 data=data[data$Date %in% c('1/2/2007','2/2/2007'),]
-# build the date-time from strings
-data$DateTime=strptime(paste(data$Date,data$Time),'%d/%m/%Y %H:%M:%S')
+# build Datetime variables from strings
+data$Datetime=strptime(paste(data$Date,data$Time),'%d/%m/%Y %H:%M:%S')
 # draw the plot
-png(filename = "plot4.png", width = 800, height = 800)
+png(filename = "plot4.png", width = 1000, height = 1000)
 par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
 with(data, {
-    plot(DateTime,Global_active_power,type='l',ylab='Global Active Power (Kilowatts)',xlab='')
-    plot(DateTime,Voltage,type='l',ylab='Voltage',xlab='',col='black')
-    plot(DateTime,Sub_metering_1,type='l',ylab='Energy sub metering',xlab='',col='black')
-    lines(DateTime,Sub_metering_2,col='red')
-    lines(DateTime,Sub_metering_3,col='blue')
+    plot(Datetime,Global_active_power,type='l',ylab='Global Active Power (Kilowatts)',xlab='')
+    plot(Datetime,Voltage,type='l',ylab='Voltage',col='black')
+    plot(Datetime,Sub_metering_1,type='l',ylab='Energy sub metering',xlab='',col='black')
+    lines(Datetime,Sub_metering_2,col='red')
+    lines(Datetime,Sub_metering_3,col='blue')
     legend("topright", pch='_', col = c("black","red","blue"), legend = paste("Sub_metering",1:3,sep='_'))
-    plot(DateTime,Global_reactive_power,type='l',ylab='Voltage',xlab='',col='black')
+    plot(Datetime,Global_reactive_power,type='l',col='black')
 })
 dev.off()
