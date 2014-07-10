8ee9ebb06220709dc4df346c1d081bfdc13f17d1
arielisr <rel.isr@gmail.com>
2014-07-10 23:01:30 +0300

    adding plot2

--- plot2.R
+++ plot2.R
@@ -6,7 +6,9 @@
 data=read.csv('D:/datasets/exploratory_data_analysis/household_power_consumption.txt',sep=';',na.strings='?')
 # select the dates 1.2.2007 and 2.2.2007
 data=data[data$Date %in% c('1/2/2007','2/2/2007'),]
+# convert the date
+data$Date=strptime(paste(data$Date,data$Time),'%d/%m/%Y')
 # draw the histogram
-png(filename = "plot1.png", width = 500, height = 500)
+#png(filename = "plot2.png", width = 500, height = 500)
 hist(data$Global_active_power,col='#ff2500',xlab='Global Active Power (Kilowatts)',main = "Global Active Power")
 dev.off()
